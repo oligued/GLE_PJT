dea36ad2bc96d478db38f74bcc5e10e8ee2d741e
Skl <sebastien.kleber@heig-vd.ch>
2014-05-26 12:15:04 +0200

    modification script pour BDD (ajout des données de test)

--- petitPateSQLQuery1.sql
+++ petitPateSQLQuery1.sql
@@ -25,10 +25,17 @@ create table Movie (
 	Category int not null references Movie_Category,
 );
 
+create table Emplacement (
+	IdPlace int not null identity (1,1) primary key,
+	Name nvarchar(20) not null,
+	Adresse nvarchar(40) not null,
+	Phone nvarchar(15) not null
+);
 create table Cinema (
 	IdCinema int not null identity (1,1) primary key,
 	Name nvarchar(20) not null,
-	Nb_Places int not null
+	Nb_Places int not null,
+	Emplacement int not null references Emplacement
 );
 
 create table Projection (
@@ -36,6 +43,7 @@ create table Projection (
 	LangVersion nvarchar(4) not null default 'VO',
 	MovieId int not null references Movie,
 	CinemaId int not null references Cinema,
+	DateHeure smalldatetime not null,
 );
 
 create table UserTable (
@@ -47,25 +55,58 @@ create table UserTable (
 	IsAdmin bit not null default 0,
 );
 
-
-create table Place (
-	IdPlace int not null identity (1,1) primary key,
-	Name nvarchar(20) not null,
-	Adresse nvarchar(40) not null,
-	Phone nvarchar(15) not null,
-	CinemaId int not null references Cinema,
-);
-
 create table Reservation (
 	IdReservation int not null identity (1,1) primary key,
-	AvailablePlaces nvarchar(20) not null,
-	ProjectionId int not null references Projection,
+	nbPlaces int not null,
+	Projection int not null references Projection,
 	UserId int not null references UserTable,
 );
 
 
 
-insert into Movie_category (IdMovieCategory, Title) values (1, 'drame');
-insert into Movie_category (IdMovieCategory, Title) values (2, 'policier');
-insert into Movie_category (IdMovieCategory, Title) values (3, 'mauvaise comédie française');
-insert into Movie_category (IdMovieCategory, Title) values (4, 'incroyable');
+insert into Movie_category (Title) values ('drame');
+insert into Movie_category (Title) values ('policier');
+insert into Movie_category (Title) values ('mauvaise comédie française');
+insert into Movie_category (Title) values ('incroyable');
+
+insert into Movie (Title, Synopsis, Producer, Category) values (
+'Dans la peau de John Malkovich',
+'"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+ tempor incididunt ut labore et dolore magna aliqua. Econsectetur 
+adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+magna aliqua."',
+'Monsieur Jean LeProd',
+4
+);
+insert into Movie (Title, Synopsis, Producer, Category) values (
+'96 heures',
+'"Carré est le patron de la BRB (Brigade de Répression du Banditisme). 3 
+ans plus tôt, il a fait tomber un grand truand, Kancel. Aujourd’hui, à 
+la faveur d’une extraction, Kancel kidnappe le flic. Il a 96 heures pour
+ lui soutirer une seule information : savoir qui l’a balancé."',
+ 'Frédéric Schoendoerffer',
+ 2
+);
+
+insert into Emplacement (Name, Adresse, Phone) values ('Vevey','Rue du milieu 1','0211231111');
+insert into Emplacement (Name, Adresse, Phone) values ('Lausanne','Route de genève 10','0214562222');
+insert into Emplacement (Name, Adresse, Phone) values ('Vevey','Rue du milieu 1','0211231111');
+
+insert into Cinema (Name, Nb_Places, Emplacement) values ('Rex 1',250,1);
+insert into Cinema (Name, Nb_Places, Emplacement) values ('Rex 2',250,2);
+insert into Cinema (Name, Nb_Places, Emplacement) values ('Rex 3',300,3);
+insert into Cinema (Name, Nb_Places, Emplacement) values ('Rex 4',200,1);
+
+insert into Projection (MovieId, CinemaId, DateHeure) values (1,2,'2014-02-13 20:45:00');
+insert into Projection (LangVersion, MovieId, CinemaId, DateHeure) values ('VF',2,3,'2014-02-13 20:45:00');
+
+insert into UserTable (UserLogin, UserPassword, FirstName, LastName, IsAdmin) values ('Cinephile1234','1234','George','Baumann',1);
+insert into UserTable (UserLogin, UserPassword, FirstName, LastName, IsAdmin) values ('Bob','1234','Dider','Burkhalter',0);
+insert into UserTable (UserLogin, UserPassword, FirstName, LastName, IsAdmin) values ('BillyTheKid','1234','Christoph','Burkhalter',0);
+
+insert into Reservation (nbPlaces, Projection,UserId) values (1,1,1);
+insert into Reservation (nbPlaces, Projection,UserId) values (2,1,2);
+insert into Reservation (nbPlaces, Projection,UserId) values (1,1,3);
+insert into Reservation (nbPlaces, Projection,UserId) values (4,2,1);
+insert into Reservation (nbPlaces, Projection,UserId) values (1,2,2);
+insert into Reservation (nbPlaces, Projection,UserId) values (5,2,3);
