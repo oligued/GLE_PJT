ef6ffad38819ee34bb21aabb9ea9717aa6188a8d
seikl <sebastien.kleber@heig-vd.ch>
2014-05-31 03:06:16 +0200

    Ajout des classes mentionnées

---
+++ ConsoleApplication1/AppAfficherDonnees.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using petitPateLib.Logic;
+
+namespace petitPate.Cmd
+{
+    class AppAfficherDonnees
+    {
+        static void Main(string[] args)
+        {
+
+            List<String> listeDesCinemas = new List<String>();
+            
+            listeDesCinemas = Cinemas.ObtenirListeCinemas(); 
+
+            // print a set of column headers
+            Console.WriteLine("Id       Nom             Nb places       Adresse                 Telephone");
+            Console.WriteLine("---      ------------    ------------    ------------------      ------------");
+
+            listeDesCinemas.ForEach(item => Console.WriteLine(item));
+
+            Console.WriteLine("Appuyer sur une touche pour quitter...");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ PortableLib/Cinemas.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace petitPateLib.Logic
+{
+    public class Cinemas
+    {
+
+        public class Cinema
+        {
+            // les variables de l'objet cinema
+            public int idCinema;
+            public string nomCinema;
+            public int nbPlaces;
+            public string adresse;
+            public string telephone;
+
+            public override string ToString()
+            {
+                return string.Format("{0}           {1}         {2}             {3}     {4}\r\n", idCinema, nomCinema, nbPlaces, adresse, telephone);
+            }
+        }
+
+
+
+
+        public static List<String> ObtenirListeCinemas()
+        {
+
+            //création d'une liste pour contenir les Cinemas;
+            List<String>  lesCinemas = new List<String>();
+
+            Cinema UnCinema = new Cinema();       
+   
+
+            // declare the SqlDataReader, which is used in
+            // both the try block and the finally block
+            SqlDataReader rdrCinema = null;
+
+            // 1. Instantiate the connection
+            SqlConnection connexion = new SqlConnection("Data Source=(local);Initial Catalog=petitPateBDD;Integrated Security=SSPI");
+
+            // create a command object
+            SqlCommand reqAfficherCinemas = new SqlCommand(
+                "select IdCinema, Cinema.Name, Nb_Places, Adresse, Phone from Cinema, Emplacement where Cinema.Emplacement = Emplacement.IdPlace", connexion);
+
+            try
+            {
+                // 2. Open the connection
+                connexion.Open();
+
+                // get query results
+                rdrCinema = reqAfficherCinemas.ExecuteReader();
+                
+                // parcours des valeurs retournées par la requête et inscription dans la liste chaînée
+                while (rdrCinema.Read())
+                {
+                        UnCinema.idCinema = (int)rdrCinema["IdCinema"];
+                        UnCinema.nomCinema = (string)rdrCinema["Name"];
+                        UnCinema.nbPlaces = (int)rdrCinema["Nb_Places"];
+                        UnCinema.adresse = (string)rdrCinema["Adresse"];
+                        UnCinema.telephone = (string)rdrCinema["Phone"];
+
+                        lesCinemas.Add(UnCinema.ToString());
+                }                
+
+                return lesCinemas; //on renvoie la liste des cinemas
+            }
+            finally
+            {
+                // close the reader
+                if (rdrCinema != null)
+                {
+                    rdrCinema.Close();
+                }
+
+                // 5. Close the connection
+                if (connexion != null)
+                {
+                    connexion.Close();
+                }
+                
+            }
+        }
+    }
+}
